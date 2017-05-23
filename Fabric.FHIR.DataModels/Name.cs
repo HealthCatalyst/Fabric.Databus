178548fc97b0a12138b672db3bebd6e9424dcec4
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-05-22 20:13:01 -0700

    remove folder checked in by mistake
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

--- Fabric.FHIR.DataModels/Name.cs
+++
@@ -1,9 +0,0 @@
-namespace Fabric.FHIR.DataModels
-{
-    public class Name
-    {
-        public string family { get; set; }
-        public string given { get; set; }
-        public string text { get; set; }
-    }
-}

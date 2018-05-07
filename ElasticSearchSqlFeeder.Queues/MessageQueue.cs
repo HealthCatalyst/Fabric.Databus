1a86208581302f8260357ef753c0968283c37be1
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-05-07 12:16:27 -0700

    update other microsoft packages

--- ElasticSearchSqlFeeder.Queues/MessageQueue.cs
+++ ElasticSearchSqlFeeder.Queues/MessageQueue.cs
@@ -71,7 +71,7 @@ namespace ElasticSearchSqlFeeder.Queues
                     channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                 };
                 channel.BasicConsume(queue: "task_queue",
-                                     noAck: false,
+                                     autoAck: false,
                                      consumer: consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");

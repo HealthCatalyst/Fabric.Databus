f338c571e39342b537e717cd91cf6d0d566a464b
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-12-04 17:33:09 -0800

    added a test webapi

---
+++ Fabric.Databus.TestApi/Controllers/ValuesController.cs
@@ -0,0 +1,89 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ValuesController.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the ValuesController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Fabric.Databus.TestApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// The values controller.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// GET api/values
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Console.WriteLine("GET");
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// GET api/values/5
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// POST api/values
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// PUT api/values/5
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        /// <summary>
+        /// DELETE api/values/5
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

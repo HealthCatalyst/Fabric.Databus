42e92e960df6361f8c6bf8629d13d476abdbac0e
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2019-01-03 16:12:55 -0800

    add a console project for running the API

---
+++ Fabric.Databus.API.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Fabric.Databus.API.Console
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            // post a validate job
+
+            // post a job
+
+        }
+    }
+}
